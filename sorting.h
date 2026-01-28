2beb17ebf91cbe77b5de74ff78dde4974b5d286f
pawaritchuenrung <pawaritchuenrung@gmail.com>
2026-01-28 08:15:51 +0000

    last

--- sorting.h
+++ sorting.h
@@ -13,13 +13,20 @@ void swap(int &a, int &b)
 void sort(int *a, int N)
 {
     int i,j;
+    int sorted;
+
     for(i=0;i<N-1;i++)
     {
+        sorted=0; // check if func swap?
         for(j=0;j<N-1;j++)
         {
             if(a[j] > a[j+1])
             swap(a[j],a[j+1]);
+            sorted=1;
         }
+        if(sorted==0)
+        break;
+    cout<<endl;
     }
 }
 //display(a,N);
