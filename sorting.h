e51c5f882274a1832d82404f5947e6dece636a32
pawaritchuenrung <pawaritchuenrung@gmail.com>
2026-01-28 07:51:07 +0000

    finished

--- sorting.h
+++ sorting.h
@@ -6,25 +6,29 @@ void swap(int &a, int &b)
 {
     int temp;
     temp = a;
-    b = a;
-    a = temp;
+    a = b;
+    b = temp;
 }
 
 void sort(int *a, int N)
 {
     int i,j;
-    //for(i=0;i<a;i++)
-    //{
-        for(j=0;j<b;j++)
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1;j++)
         {
             if(a[j] > a[j+1])
-            swap(a[j],a[j+1])
+            swap(a[j],a[j+1]);
         }
-    //}
+    }
 }
 //display(a,N);
 
-void display()
+void display(int *a,int N)
 {
-
+    int i;
+    for(i=0;i<N;i++)
+    {
+        cout<< a[i] <<" ";
+    }
 }
